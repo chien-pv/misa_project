10bb602e1a0efb8cabfd3e5a1b616e92dc48a4e8
tankietkt <tankietkt@gmail.com>
2016-05-08 21:11:26 +0700

    update input item

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -10,12 +10,40 @@ class ItemsController < ApplicationController
   end
 
   def insert
-    @list_items = InventoryItem.where(InventoryItemID: params[:list_items])
-    @list_items.each do |item|
-      params_create = {item_id: item.InventoryItemID,
-                       item_code: item.InventoryItemCode,
-                       supplier_id: params[:supplier_id]}
-      Item.create(params_create)
+    list_items = InventoryItem.where(InventoryItemID: params[:list_items]) #lisst truyen len
+    list_supplier= Item.where(supplier_id: params[:supplier_id]) #list mat hang theo nha cung cap da co
+    if list_supplier.blank?
+      list_items.each do |item|
+        params_create = {item_id: item.InventoryItemID,
+                         item_code: item.InventoryItemCode,
+                         supplier_id: params[:supplier_id]}
+        Item.create(params_create)
+      end
+    else
+      # binding.pry
+      temp = list_supplier.where(item_id: params[:list_items]) # mat hang da co theo nha cung cap voi item truyen len 
+      list_item_id = temp.map(&:item_id) #tao mang item_id cua item da co
+      list_temp = params[:list_items] - list_item_id #truyen len ma chua co
+      list_item_not_select = list_supplier.map(&:item_id)
+      list_temp_not_params = list_item_not_select - list_item_id
+        if list_temp.present?
+          list_items = InventoryItem.where(InventoryItemID: list_temp)
+          list_items.each do |item|
+            params_create = {item_id: item.InventoryItemID,
+                             item_code: item.InventoryItemCode,
+                             supplier_id: params[:supplier_id]}
+            if item_search = Item.find_by(item_id: item.InventoryItemID, supplier_id: nil).present?
+              item_search.update(supplier_id: params[:supplier_id] )
+            else
+              Item.create(params_create)
+            end
+          end
+        end
+      # binding.pry
+      if list_temp_not_params.present?
+         # binding.pry
+        Item.where(item_id: list_temp_not_params).where(supplier_id: params[:supplier_id]).destroy_all
+      end
     end
     redirect_to item_path(params[:supplier_id])
   end
