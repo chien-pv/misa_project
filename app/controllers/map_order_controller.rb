6cce511df7071fea4e93e260bdfba88809ef6344
VanChien <chienpv@asiantech.vn>
2016-05-09 17:27:00 +0700

    fix save map order

--- app/controllers/map_order_controller.rb
+++ app/controllers/map_order_controller.rb
@@ -11,11 +11,13 @@ class MapOrderController < ApplicationController
   def creates
     map_id = Time.now.strftime("%Y%d%m%H%M%S")
     params[:Pu].each do |pu|
+      # binding.pry
       MapPurchase.create(map_id: map_id , RefID: pu[:RefID], date_map: Time.now.strftime("%Y-%d-%m %H:%M:%S %Z"))
       pu[:Item].each do |item|
-        MapItem.create(ItemID: item[:InventoryItemID], map_purchase_id: MapPurchase.find_by(RefID: pu[:RefID]).id, quantity: item[:quantity]  )
+        MapItem.create(ItemID: item[:InventoryItemID], map_purchase_id: MapPurchase.find_by(RefID: pu[:RefID], map_id: map_id).id, quantity: item[:quantity]  )
       end
     end
+    redirect_to map_order_index_path
   end
 
 end
