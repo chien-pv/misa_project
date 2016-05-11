f3ff9cee17731c41c97d2e114a23abdb87b5bf95
VanChien <chienpv@asiantech.vn>
2016-05-11 18:36:40 +0700

    fix show button

--- app/controllers/show_map_controller.rb
+++ app/controllers/show_map_controller.rb
@@ -1,5 +1,6 @@
 class ShowMapController < ApplicationController
   def index
+    @list_unit = Unit.all
     @list_map = MapPurchase.all.map(&:map_id).uniq
     if params[:list_map].nil?
       @list_order = MapPurchase.where( map_id: @list_map.first)
